9f78973e805d9deba512a7c454822f15ada19044
Vishal Bind <137315909+vishaljbind@users.noreply.github.com>
2025-01-01 22:09:11 +0530

    Project Completed

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
+import commentRoutes from "./routes/comment.route.js"
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
+const app = express()
+
+// for allowing json object in req body
+app.use(express.json())
+app.use(cookieParser())
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000!")
+})
+
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/post", postRoutes)
+app.use("/api/comment", commentRoutes)
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+
+  const message = err.message || "Internal Server Error"
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
